d740fcbc36addb177d0844db7025a1c1697cbc2b
Hanna Negash <hnegash.n@gmail.com>
2023-09-12 07:00:36 -0700

    Practice with variable and scope errors

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Write your solution in this file!
+//Don't use var and all of these are wrong but to understand
+
+var customerName = "bob";
+const leastFavoriteCustomer = "Will";
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+  bestCustomer = "not bob";
+}
+
+function overwriteBestCustomer() {
+  bestCustomer = "maybe bob";
+}
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = "Bobby";
+}
